1c26c4608674a0c6c7ee5a964477a223ac8aad38
vivienduple <viduple@gmail.com>
2017-03-31 15:06:17 +0200

    Intégration des migrations de la base de données initiale.

---
+++ database/migrations/2017_03_31_120128_create_avatars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAvatarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avatars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('email');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('avatars', function (Blueprint $table) {
+            Schema::dropIfExists('avatars');
+        });
+    }
+}
